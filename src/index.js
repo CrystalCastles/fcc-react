af7cc60d3e4d8e8c12a3d16281dc98e1d9bc8038
Daniel Pichardo <danielpichardo716@gmail.com>
2017-08-18 23:29:50 +0000

    Set primary custom color

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import { MuiThemeProvider } from 'material-ui/styles'
+import { MuiThemeProvider } from 'material-ui/styles';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: '#f48942',
+  },
+  appBar: {
+    height: 50,
+  },
+});
 
 ReactDOM.render(
-  <MuiThemeProvider><App /></MuiThemeProvider>,
+  <MuiThemeProvider muiTheme={muiTheme}><App /></MuiThemeProvider>,
   document.getElementById('root')
 );
